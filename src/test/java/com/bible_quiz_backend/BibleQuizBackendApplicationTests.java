79daebfb40f567845917a78d7ec6ac6bcf4b0702
hingjae <ljh23270@naver.com>
2025-08-15 22:31:27 +0900

    Integration test disabled

--- src/test/java/com/bible_quiz_backend/BibleQuizBackendApplicationTests.java
+++ src/test/java/com/bible_quiz_backend/BibleQuizBackendApplicationTests.java
@@ -1,9 +1,11 @@
 package com.bible_quiz_backend;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+@Disabled
 class BibleQuizBackendApplicationTests extends IntegrationTest{
 
 	@Test
