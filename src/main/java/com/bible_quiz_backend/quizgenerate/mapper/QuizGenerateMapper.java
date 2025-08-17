a9eaae5928f64c3c00547c36e91e6aa757266106
hingjae <ljh23270@naver.com>
2025-08-18 01:31:36 +0900

    sqs 분리

--- src/main/java/com/bible_quiz_backend/quizgenerate/mapper/QuizGenerateMapper.java
+++ src/main/java/com/bible_quiz_backend/quizgenerate/mapper/QuizGenerateMapper.java
@@ -3,7 +3,7 @@ package com.bible_quiz_backend.quizgenerate.mapper;
 import com.bible_quiz_backend.common.utils.JsonUtils;
 import com.bible_quiz_backend.quiz.domain.Quiz;
 import com.bible_quiz_backend.quizgenerate.dto.QuizGenerateMessage;
-import com.bible_quiz_backend.quizgenerate.dto.QuizGenerateMessageProduceBody;
+import com.bible_quiz_backend.quizgenerate.dto.QuizGenerateProduceMessageBody;
 import com.bible_quiz_backend.quizgenerate.dto.QuizGenerateResponse;
 import com.bible_quiz_backend.topic.domain.Topic;
 import org.springframework.stereotype.Component;
@@ -13,8 +13,9 @@ import java.util.List;
 @Component
 public class QuizGenerateMapper {
 
-    public QuizGenerateMessageProduceBody toQuizGenerateMessageBody(Topic topic) {
-        return QuizGenerateMessageProduceBody.of(
+    public QuizGenerateProduceMessageBody toQuizGenerateMessageBody(String resultQueueUrl, Topic topic) {
+        return QuizGenerateProduceMessageBody.of(
+                resultQueueUrl,
                 topic.getId(),
                 topic.getBookFileName(),
                 topic.getQuestion()
--- src/main/java/com/bible_quiz_backend/quizgenerate/testcontroller/TestQuizGenerateController.java
+++ src/main/java/com/bible_quiz_backend/quizgenerate/testcontroller/TestQuizGenerateController.java
@@ -1,12 +1,13 @@
 package com.bible_quiz_backend.quizgenerate.testcontroller;
 
-import com.bible_quiz_backend.quizgenerate.dto.QuizGenerateMessageProduceBody;
+import com.bible_quiz_backend.quizgenerate.dto.QuizGenerateProduceMessageBody;
 import com.bible_quiz_backend.quizgenerate.dto.QuizGenerateRequest;
 import com.bible_quiz_backend.quizgenerate.mapper.QuizGenerateMapper;
 import com.bible_quiz_backend.quizgenerate.service.QuizGenerateProducer;
 import com.bible_quiz_backend.topic.domain.Topic;
 import com.bible_quiz_backend.topic.service.TopicService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,11 +22,14 @@ public class TestQuizGenerateController {
     private final QuizGenerateMapper quizGenerateMapper;
     private final QuizGenerateProducer quizGenerateProducer;
 
+    @Value("${aws.quiz-result-queue-url}")
+    private String resultQueueUrl;
+
     @PostMapping
     public String generateQuiz(@RequestBody QuizGenerateRequest request) {
         Topic topic = topicService.findById(request.topicId());
-        QuizGenerateMessageProduceBody quizGenerateMessageProduceBody = quizGenerateMapper.toQuizGenerateMessageBody(topic);
-        quizGenerateProducer.send(quizGenerateMessageProduceBody);
+        QuizGenerateProduceMessageBody quizGenerateProduceMessageBody = quizGenerateMapper.toQuizGenerateMessageBody(resultQueueUrl, topic);
+        quizGenerateProducer.send(quizGenerateProduceMessageBody);
         return "success";
     }
 }
